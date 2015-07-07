65aa7c6361ad86f306ea79ccdcf9a91fea79925f
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-07-07 10:29:01 -0600

    add route to edit

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,7 +2,6 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
-
   get '/' do
     erb :dashboard
   end
@@ -24,5 +23,14 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id' do |id|
     @task = TaskManager.find(id.to_i)
     erb :show
+  end
+
+  get '/tasks/:id/edit' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  not_found do
+    erb :error
   end
 end
