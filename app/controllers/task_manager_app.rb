894cd9588d0c2d91ba8f822b57d2db72763cf0d9
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-07-08 10:40:40 -0600

    updates due to config/

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,5 @@
-require 'models/task_manager'
-
 class TaskManagerApp < Sinatra::Base
-  set :root, File.join(File.dirname(__FILE__), '..')
+
   get '/' do
     erb :dashboard
   end
@@ -28,6 +26,16 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id/edit' do |id|
     @task = TaskManager.find(id.to_i)
     erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    TaskManager.update(id.to_i, params[:task])
+    redirect "tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
   end
 
   not_found do
--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -1,19 +1,22 @@
 require 'yaml/store'
-require_relative 'task'
 
 class TaskManager
   def self.database
-    @database ||=  YAML::Store.new('db/task_manager')
+    if ENV["TASK_MANAGER_ENV"] == 'test'
+      @database ||= YAML::Store.new("db/task_manager_test")
+    else
+      @database ||= YAML::Store.new("db/task_manager")
+    end
   end
 
   def self.create(task)
-    database.transaction do
-      database['tasks'] ||= []
-      database['total'] ||= 0
-      database['total'] += 1
-      database['tasks'] << { "id" => database['total'],
-                            "title" => task[:title],
-                            "description" => task[:description] }
+      database.transaction do
+        database['tasks'] ||= []
+        database['total'] ||= 0
+        database['total'] += 1
+        database['tasks'] << { "id" => database['total'],
+                              "title" => task[:title],
+                              "description" => task[:description] }
     end
   end
 
@@ -33,5 +36,26 @@ class TaskManager
 
   def self.find(id)
     Task.new(raw_task(id))
+  end
+
+  def self.update(id, task)
+    database.transaction do
+      target = database["tasks"].find { |data| data["id"] == id }
+      target["title"] = task[:title]
+      target["description"] = task[:description]
+    end
+  end
+
+  def self.delete(id)
+    database.transaction do
+      database["tasks"].delete_if { |task| task["id"] == id }
+    end
+  end
+
+  def self.delete_all
+    database.transaction do
+      database["tasks"] = []
+      database["total"] = 0
+    end
   end
 end
