682332c1577790cb45c813dce00704f740517fe8
Andrew Carmer <carmerandrew@gmail.com>
2016-06-29 13:28:36 -0600

    start

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ ENV['RACK_ENV'] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
-# require 'tilt/erb'
 
 module TestHelpers
   def teardown
